17b81668c17bca6fa57accb3a7f6d5be2f348b29
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-05-06 20:44:48 -0400

    update prod config

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :mesh_relay, MeshRelay.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [port: 80]
+  url: [scheme: "https", host: System.get_env("WEB_SOCKET_URL"), port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  # cache_static_manifest: "priv/static/manifest.json",
+  # secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
